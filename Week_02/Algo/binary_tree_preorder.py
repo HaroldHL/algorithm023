8bb5c0062fb992e0bfd86b9aceb82726cf2c6320
HaroldYin <haroldyin@outlook.com>
2021-01-08 17:28:18 +0800

    update Week2

---
+++ Week_02/Algo/binary_tree_preorder.py
@@ -0,0 +1,26 @@
+ def preorderTraversal(self, root: TreeNode) -> List[int]:
+        # 递归的方式类似模板 前序是根-左-右
+        def preorderTraversal(self, root: TreeNode) -> List[int]:
+            res = []
+            def dfs(root):
+                if not root:
+                    return False
+                res.append(root.val)
+                dfs(root.left)
+                dfs(root.right)
+            dfs(root)
+            return res  
+        #iterative solution
+        def preorderTraversal(self, root: TreeNode) -> List[int]:
+            if not root:return []
+            stack, res = [root],[]
+            while stack:
+                node = stack.pop()
+                if node:
+                    res.append(node.val)
+                    if node.right:
+                        stack.append(node.right)
+                    if node.left:
+                        stack.append(node.left)
+            return res
+
--- Week_02/README.md
+++ Week_02/README.md
@@ -5,3 +5,43 @@
 * possible solution --> optimal(time & space)
 * code
 * test
+
+
+二叉树
+如果你按照 根节点 -> 左孩子 -> 右孩子 的方式遍历，即「先序遍历」
+左孩子 -> 根节点 -> 右孩子 的方式遍历，即「中序序遍历」
+左孩子 -> 右孩子 -> 根节点 的方式遍历，即「后序序遍历」
+
+三者都有一个固定的模板，只需将递归函数里的 res.append(root.val) 放在不同位置即可，然后调用这个递归函数就可以了，代码完全一样。
+
+```python
+def dfs(root):
+  res = []
+  if not root:
+    return
+  
+  res.append(root.val) #前序
+  dfs(root.left)
+  dfs(root.right)
+
+dfs(root)
+return res
+```
+
+迭代的模板
+
+```python
+def preorderTraversal(self, root: TreeNode) -> List[int]:
+        if not root:return []
+        stack, res = [root],[]
+        while stack:
+            node = stack.pop()
+            if node:
+                res.append(node.val)
+                if node.right:
+                    stack.append(node.right)
+                if node.left:
+                    stack.append(node.left)
+        return res
+```
+
