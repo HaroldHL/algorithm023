4417192cf807e0c8cd963d2e77dc2b18e8667162
HaroldYin <haroldyin@outlook.com>
2021-03-07 22:27:23 +0800

    week9

--- Week_09/Algo/Microsoft.py
+++ Week_09/Algo/Microsoft.py
@@ -48,8 +48,30 @@ def isIsomorphic(s,t):
         if (hashmaps.get(s[i]) != hashmapt.get(t[i])): return False
 
     return True
+
+def expandAroundCenter(s,left,right):
+    while left >= 0 and right < len(s) and s[left] == s[right]:
+        left -= 1
+        right += 1
+    return left + 1, right - 1
+
+def longestPalindrome(s):
+    start,end = 0,0
+    for i in range(len(s)):
+        left1,right1 = expandAroundCenter(s,i,i)
+        left2,right2 = expandAroundCenter(s,i,i+1)
+    
+        if right1-left1 > end-start:
+            start,end = left1,right1
+        if right2-left2 >end- start:
+            start,end = left2,right2
+    return s[start:end+1]
+        
+        
 if __name__ == '__main__':
     print(myAtoi("42"))
     print(firstUniqChar("leetcode"))
     print(reverseWords("the sky is blue"))
     print(reverseWords3("Let's take LeetCode contest"))
+    print(longestPalindrome("babad"))
+    print(longestPalindrome("cbbd"))
