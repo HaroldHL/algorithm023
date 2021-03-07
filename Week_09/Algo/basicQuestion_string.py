2907800f9403b4486320b27f51e738dee300b90a
HaroldYin <haroldyin@outlook.com>
2021-03-08 00:42:49 +0800

    update week9

--- Week_09/Algo/basicQuestion_string.py
+++ Week_09/Algo/basicQuestion_string.py
@@ -67,9 +67,27 @@ def validPalindrome2(s):
             j -=1
         return True
     
+def isMatch(s,p):
+    ls, lp = len(s), len(p)
+    dp = [[False for _ in range(lp + 1)] for _ in range(ls + 1)]
+    dp[0][0] = True
+    for j in range(2, lp + 1):
+        dp[0][j] = dp[0][j - 2] and p[j - 1] == '*'
+    for i in range(1, ls + 1):
+        for j in range(1, lp + 1):
+            m, n = i - 1, j - 1
+            if p[n] == '*':
+                if s[m] == p[n - 1] or p[n - 1] == '.':
+                    dp[i][j] = dp[i][j - 2] or dp[i - 1][j]
+                else: dp[i][j] = dp[i][j - 2]
+            elif s[m] == p[n] or p[n] == '.':
+                dp[i][j] = dp[i - 1][j - 1]
+    return dp[-1][-1]
+    
 if __name__=='__main__':
     str1 = 'Hello'
     print(toLowerCase(str1))
     print(numJewelsInStones("aA","aAAbbbb"))
     print(firstUniqChar("loveleetcode"))
     print(reverseOnlyLetters("ab-cd"))
+    print(isMatch("aa",'a'))
